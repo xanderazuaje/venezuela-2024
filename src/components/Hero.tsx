47c44ada46b7d8f1acf3d301698dbc7a8760c3dc
jonpena <gran_200014@hotmail.com>
2024-08-01 01:43:20 +0200

    refactor: :art: Se creo el componente Hero.tsx como parte del refactor

---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+const Hero = () => {
+  return (
+    <header class="flex flex-col sm:flex-row items-center w-full justify-between">
+      <h1 class="font-bold text-[50px] max-w-[608px] leading-[60px]">
+        Todo lo que necesitas saber sobre tu país. Venezuela.
+      </h1>
+      <img src="/vzla.svg" />
+    </header>
+  );
+};
+
+export default Hero;
