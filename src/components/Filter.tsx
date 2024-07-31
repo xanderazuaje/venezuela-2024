9f8c02a31a2797c2fb079480ab0b95efce097bf3
Xander Azuaje <charon-mortis@pm.me>
2024-07-31 06:55:54 +0200

    responsive

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -11,22 +11,25 @@ export default function Filter({elements}: Props) {
 
     return (
         <>
-        <div class="flex justify-center gap-[30px] my-[30px] text-[25px] items-center">
+        <div class="flex flex-col md:flex-row justify-center gap-[30px] my-[30px] text-[25px] items-center">
             <button onClick={() => setFilter(null)} class={`block btn cursor-pointer ${filter === null ? 'selected-btn' : ''}`}>Todos</button>
             {options.map(({type}) => (
                 <>
-                <hr class="h-[40px] w-[1px] border-0 bg-black"/>
+                <hr class="h-[40px] w-[1px] border-0 bg-black hidden md:block"/>
                 <button onClick={() => setFilter(type)} class={`block btn cursor-pointer capitalize ${filter === type ? 'selected-btn' : ''}`}>{type}</button>
                 </>
             ))}
         </div>
-        <div class="grid grid-cols-3 gap-y-[30px]">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-y-[30px]">
             {elements.filter(({type}) => type === filter || filter === null).map((e) => (
                 <div class="flex flex-col justify-center">
                     <img src={`/${e.type}.svg`} alt={e.type} class="w-[60px] m-auto"/>
                     <p class="capitalize text-center">{e.region}</p>
                     <p class="capitalize text-center text-[20px] font-bold">{e.name}</p>
-                    <a href={`tel:${e.phone}`} class="block m-auto text-[#D00B27] font-sans font-bold">{e.phone}</a>
+                    <div class="flex gap-[10px] m-auto">
+                        <img src="/phone.svg" alt=""/>
+                        <a href={`tel:${e.phone}`} class="block text-[#D00B27] font-sans font-bold">{e.phone}</a>
+                    </div>
                     <p class="text-center">{e.description}</p>
                     <div>
                     </div>
