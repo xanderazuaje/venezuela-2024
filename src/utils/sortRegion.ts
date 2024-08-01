39827d20ed902b55a9c0fa718e75802eec26dd61
jonpena <gran_200014@hotmail.com>
2024-08-01 09:30:17 +0200

    docs: :rocket: se agrego sus types o interfaces

--- src/utils/sortRegion.ts
+++ src/utils/sortRegion.ts
@@ -1,4 +1,6 @@
-export const sortRegion = (regions: Array<any>) => {
+import type { RegionProps } from "@/interfaces/region.interface";
+
+export const sortRegion = (regions: RegionProps[]) => {
   return regions.sort((a, b) => {
     if (a.region < b.region) return -1;
     if (a.region > b.region) return 1;
