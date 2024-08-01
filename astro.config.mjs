754c8b28490d2fd5db95de78a8253ae6d15dbdc3
Xander Azuaje <charon-mortis@pm.me>
2024-08-01 18:47:40 +0200

    revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 
+import auth from "auth-astro";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()],
+  integrations: [tailwind(), preact(), auth()],
   output: "server",
   adapter: cloudflare(),
 });
