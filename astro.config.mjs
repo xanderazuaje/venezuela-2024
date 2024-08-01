74b3a2d3323a68e439abfbc2fe9d95d2a3cf67c4
Xander Azuaje <charon-mortis@pm.me>
2024-08-01 18:07:43 +0200

    uninstalled auth-astro package

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 
-import auth from "auth-astro";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), auth()],
+  integrations: [tailwind(), preact()],
   output: "server",
   adapter: cloudflare(),
 });
