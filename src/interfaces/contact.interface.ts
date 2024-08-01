354235ba338c4735d0017f1d561d0464c34a009b
jonpena <gran_200014@hotmail.com>
2024-08-01 09:28:52 +0200

    feat: :art: Se creo la interface contact para tipar sus variables

---
+++ src/interfaces/contact.interface.ts
@@ -0,0 +1,13 @@
+export interface ContactProps {
+  id: number;
+  name: string;
+  type: string;
+  description: string | null;
+  phone: number;
+  phoneNum: string;
+  region: string;
+  telegram: string | null;
+  instagram: string | null;
+  whatsapp: string | null;
+  twitter: string | null;
+}
