def3a0fc809629b0b584ada9b9521d2f407679a2
Xander Azuaje <charon-mortis@pm.me>
2024-08-02 04:27:39 +0200

    netlitfy deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,12 @@
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 
-// https://astro.build/config
+import netlify from "@astrojs/netlify";
 
+// https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), preact()],
-    output: "server",
-    vite: {
-      define: {
-          "process.env.SUPABASE_URL": JSON.stringify(process.env.SUPABASE_URL),
-          "process.env.SUPABASE_ANON_KEY": JSON.stringify(process.env.SUPABASE_ANON_KEY)
-      }
-    },
-    adapter: cloudflare({
-        mode: "directory"
-    })
+  integrations: [tailwind(), preact()],
+  output: "server",
+  adapter: netlify()
 });
