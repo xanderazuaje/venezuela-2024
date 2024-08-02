486027fb323ffcf4010e18aba78b82d7fc7dee78
Xander Azuaje <charon-mortis@pm.me>
2024-08-02 04:11:47 +0200

    redefined env variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,8 @@ export default defineConfig({
     output: "server",
     vite: {
       define: {
-          "process.env.SUPABASE_URL": process.env.SUPABASE_URL,
-          "process.env.SUPABASE_ANON_KEY": process.env.SUPABASE_ANON_KEY
+          "process.env.SUPABASE_URL": JSON.stringify(process.env.SUPABASE_URL),
+          "process.env.SUPABASE_ANON_KEY": JSON.stringify(process.env.SUPABASE_ANON_KEY)
       }
     },
     adapter: cloudflare({
