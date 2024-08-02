8dbcb5b1cead3acf62641e6d8cb92fc4c2d10e81
Xander Azuaje <charon-mortis@pm.me>
2024-08-02 02:40:06 +0200

    debug exception

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 
+const url = import.meta.env.SUPABASE_URL
+const key = import.meta.env.SUPABASE_ANON_KEY
+
+if (!url)
+    throw new Error("URL is required")
+if (!key)
+    throw new Error("Key is required")
+
 export const supabase = createClient(
-    import.meta.env.SUPABASE_URL,
-    import.meta.env.SUPABASE_ANON_KEY,
+    url,
+    key
 );
