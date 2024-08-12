0c331d6d08207c6a3e14a0fb092f08ddba4a2e01
Xander Azuaje <charon-mortis@pm.me>
2024-08-12 09:40:12 +0200

    added uuidv4 identifyer to images

--- src/pages/api/donations/create.ts
+++ src/pages/api/donations/create.ts
@@ -8,16 +8,26 @@ async function imgToWebp(imageFile: Blob): Promise<Buffer> {
     try {
         const buffer = Buffer.from(await imageFile.arrayBuffer());
         return await sharp(buffer)
-            .webp({quality: 80}) // Adjust quality as needed
+            .webp({quality: 80})
+            .resize({
+                width: 300,
+                height: 300,
+            })
             .toBuffer();
     } catch (error) {
         throw new Error('Failed to convert image to WebP format');
     }
 }
 
-async function uploadImage(imageFile: Blob, fileName: string) {
+function uuidv4(): string {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+        (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+    );
+}
+
+async function uploadImage(imageFile: Blob) {
     const webpImg = await imgToWebp(imageFile);
-    const path = `public/${fileName.split('.').slice(0, -1).join('.')}.webp`;
+    const path = `public/${uuidv4()}.webp`;
     const { data, error } = await supabase
         .storage
         .from(bucketName)
@@ -47,7 +57,7 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
         const req = {
             name: formData.get("name"),
             url: formData.get("url"),
-            picture: img ? await uploadImage(img, img.name) : null,
+            picture: img ? await uploadImage(img) : null,
             description: formData.get("description"),
             type: formData.get("type"),
             created_by: data.session.user?.email
