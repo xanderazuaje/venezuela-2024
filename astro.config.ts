8687516ae3dafbd1a751ce786b773e73f60a6a6e
Daniel García <workspacedaniel2121@gmail.com>
2024-08-26 15:40:45 -0400

    moreInfoButton & sliders buttons

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: "server",
-  adapter: netlify(),
+  adapter: netlify()
 });
